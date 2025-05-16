5d474418588bb670b7b5bd42d15d10cd899a3587
jsstoni <jsstoniha@gmail.com>
2025-05-16 18:37:58 -0400

    integration page

---
+++ app/dashboard/integration/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/components/dashboard/header';
+
+export default function Integration() {
+  return (
+    <>
+      <Header>
+        <h1>Integration</h1>
+      </Header>
+      Integration
+    </>
+  );
+}
