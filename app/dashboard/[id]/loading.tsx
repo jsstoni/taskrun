7dcd0f6a9bf057deadb692846a5751bc6b0782c3
jsstoni <jsstoniha@gmail.com>
2025-05-26 16:18:50 -0400

    loader

--- app/dashboard/[id]/loading.tsx
+++ app/dashboard/[id]/loading.tsx
@@ -8,13 +8,14 @@ export default function LoadingState() {
           <Skeleton className="h-6 w-6" />
           <Skeleton className="h-5 w-24" />
         </div>
-        <Skeleton className="mr-2 ml-auto h-8 w-24 bg-destructive" />
-        <Skeleton className="h-8 w-24 bg-primary" />
       </div>
 
       <div className="mt-2 flex items-center border-b px-0 py-4">
         <Skeleton className="mr-2 h-12 w-12" />
         <Skeleton className="h-6 w-16" />
+
+        <Skeleton className="mr-2 ml-auto h-8 w-24" />
+        <Skeleton className="h-8 w-24 bg-primary" />
       </div>
 
       <div className="mt-4 space-y-4">
