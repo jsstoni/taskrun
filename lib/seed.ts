cd900adbb71ec63c3827395fa744aa17e16ab957
jsstoni <jsstoniha@gmail.com>
2025-06-01 01:03:25 -0400

    Include discord info in job notifications

--- lib/seed.ts
+++ lib/seed.ts
@@ -4,11 +4,22 @@ import { sql } from 'drizzle-orm';
 (async () => {
   await db.execute(sql`
     CREATE OR REPLACE FUNCTION notify_new_job() RETURNS trigger AS $$
+    DECLARE
+      discord_record json;
     BEGIN
+      SELECT row_to_json(d)
+      INTO discord_record
+      FROM discords d
+      WHERE d.user_id = NEW.user_id;
+
       PERFORM pg_notify(
         'new_job',
-        row_to_json(NEW)::text
+        json_build_object(
+          'job', row_to_json(NEW),
+          'discord', discord_record
+        )::text
       );
+
       RETURN NEW;
     END;
     $$ LANGUAGE plpgsql;
